1aaf4d193d66729c2381002857c0c8642442ca10
German G Sayago <german.sayago@gmail.com>
2020-08-03 15:47:34 -0300

    actualizacion de .env

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,15 +1,10 @@
 import React, { Component } from 'react';
-import Logo from '../components/Logo';
 
 class Home extends Component {
   render () {
     return (
       <div className="container">
-        <div className="row justify-content-center">
-          <div className="col-8 col md-3">
-            <Logo />
-          </div>
-        </div>
+        Home
       </div>
     )
   }
