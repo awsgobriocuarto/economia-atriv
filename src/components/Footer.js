f6b206063fe3177ead442306b17a868901b7da5d
German G Sayago <german.sayago@gmail.com>
2020-09-10 09:37:23 -0300

    se removieron los iconos importados en el footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,4 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFacebook, faInstagram } from "@fortawesome/free-brands-svg-icons";
 
 const Footer = (props) => (
   <footer>
